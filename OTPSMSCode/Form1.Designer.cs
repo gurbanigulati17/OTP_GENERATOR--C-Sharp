7c50d4f7ef495b295e8a4c28920fd38e6824dc0e
Gurubani Gulati <gurbanigulati17@gmail.com>
2020-09-07 21:52:44 +0530

    Add files via upload

---
+++ OTPSMSCode/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace OTPSMSCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtVerOTP = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnVerify = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(150, 108);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(6);
+            this.label1.Size = new System.Drawing.Size(125, 43);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cust ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(150, 179);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(6);
+            this.label2.Size = new System.Drawing.Size(221, 43);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mobile Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Green;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(150, 327);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(7);
+            this.label3.Size = new System.Drawing.Size(171, 45);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Verify OTP";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(389, 108);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(190, 43);
+            this.txtName.TabIndex = 3;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPhone.Location = new System.Drawing.Point(389, 179);
+            this.txtPhone.Multiline = true;
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(190, 43);
+            this.txtPhone.TabIndex = 4;
+            // 
+            // txtVerOTP
+            // 
+            this.txtVerOTP.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtVerOTP.Location = new System.Drawing.Point(375, 327);
+            this.txtVerOTP.Multiline = true;
+            this.txtVerOTP.Name = "txtVerOTP";
+            this.txtVerOTP.Size = new System.Drawing.Size(189, 45);
+            this.txtVerOTP.TabIndex = 5;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.btnLogin.Location = new System.Drawing.Point(473, 228);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(106, 45);
+            this.btnLogin.TabIndex = 6;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnVerify
+            // 
+            this.btnVerify.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnVerify.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVerify.ForeColor = System.Drawing.Color.Black;
+            this.btnVerify.Location = new System.Drawing.Point(551, 327);
+            this.btnVerify.Name = "btnVerify";
+            this.btnVerify.Size = new System.Drawing.Size(94, 45);
+            this.btnVerify.TabIndex = 7;
+            this.btnVerify.Text = "Submit";
+            this.btnVerify.UseVisualStyleBackColor = false;
+            this.btnVerify.Click += new System.EventHandler(this.btnVerify_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnVerify);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtVerOTP);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.Name = "Form1";
+            this.Text = "OTPCODE";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtVerOTP;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnVerify;
+    }
+}
+
---
+++ OTPSMSCode/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Collections.Specialized;
+using System.IO;
+
+namespace OTPSMSCode
+{
+    public partial class Form1 : Form
+    {
+		string randomNumber;
+		public Form1()
+        {
+			
+            InitializeComponent();
+        }
+
+		private void btnLogin_Click(object sender, EventArgs e)
+        {
+			String result;
+			string apiKey = "V0JrtO6uz2c-hXmUhFgrdSLsMW8OS9CaeDNOFXcCON";
+			string numbers = txtPhone.Text; // in a comma seperated list
+			 sender = "GEET";
+			string name = txtName.Text;
+			Random rnd = new Random();
+			randomNumber = (rnd.Next(100000,9999999)).ToString();
+			Console.WriteLine(randomNumber);
+			//string message = "Hey"+name+"Your OTP is"+randomNumber;
+			string message = "Hey "+name+" Your OTP is " + randomNumber;
+			Console.WriteLine(sender);
+			Console.WriteLine(message);
+
+			//String url = "https://api.textlocal.com/send/?apikey="+apiKey +"&numbers="+ numbers +"&message="+message+"&sender="+sender;
+			//refer to parameters to complete correct url string
+			String url = "https://api.txtlocal.com/send/?apikey=" + apiKey + "&numbers=" + numbers + "&message=" + message + "&sender=" + sender;
+			Console.WriteLine(url);
+			StreamWriter myWriter = null;
+			HttpWebRequest objRequest = (HttpWebRequest)WebRequest.Create(url);
+
+			objRequest.Method = "POST";
+			objRequest.ContentLength = Encoding.UTF8.GetByteCount(url);
+			objRequest.ContentType = "application/x-www-form-urlencoded";
+			try
+			{
+				myWriter = new StreamWriter(objRequest.GetRequestStream());
+				myWriter.Write(url);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+			finally
+			{
+				myWriter.Close();
+			}
+
+			HttpWebResponse objResponse = (HttpWebResponse)objRequest.GetResponse();
+			using (StreamReader sr = new StreamReader(objResponse.GetResponseStream()))
+			{
+				result = sr.ReadToEnd();
+				// Close and clean up the StreamReader
+				sr.Close();
+			}
+	
+		MessageBox.Show("OTP send successfully "+message);
+
+		}
+
+	/*	private void btnLogin_Click(object sender, EventArgs e)
+        {
+			string customerId = "71E73325-1C5C-4196-881E-D3750AD05C40";
+			string apiKey = "WybgrNsoDPaZ2z6m0HqoEeOcw8eWl+PFUWtTtNXv4yaacb9nYiU2FF42GuOGqaF7pFYPKFW06EMYvF7RVDM0VQ==";
+
+			string phoneNumber = txtPhone.Text;
+
+			string message = "You're scheduled for a dentist appointment at 2:30PM.";
+			string messageType = "ARN";
+
+			try
+			{
+				MessagingClient messagingClient = new MessagingClient(customerId, apiKey);
+				RestClient.TelesignResponse telesignResponse = messagingClient.Message(phoneNumber, message, messageType);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+			}
+
+			Console.WriteLine("Press any key to quit.");
+			Console.ReadKey();
+		}*/
+
+
+		private void btnVerify_Click(object sender, EventArgs e)
+        {
+			if(txtVerOTP.Text == randomNumber)
+            {
+				MessageBox.Show("Login Successfully");
+            }
+            else
+            {
+				MessageBox.Show("Wrong OTP. Try Again");
+            }
+        }
+
+    }
+}
